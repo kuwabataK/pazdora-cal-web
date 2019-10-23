de6c2c1990711845f000f4f2543bce549184224a
KuwabataK <monorod98@gmail.com>
2019-10-23 22:07:15 +0900

    precacheの中身も置換するようにした

--- replace-bm.js
+++ replace-bm.js
@@ -1,15 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs')
 
-const filePath = 'out/build-manifest.json'
+function replaceStatic(filePath) {
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err)
+    }
+    const result = data.replace(/static/g, '_next/static')
 
-fs.readFile(filePath, 'utf8', function(err, data) {
-  if (err) {
-    return console.log(err)
-  }
-  const result = data.replace(/static/g, '_next/static')
+    fs.writeFile(filePath, result, 'utf8', function(err) {
+      if (err) return console.log(err)
+    })
+  })
+}
 
-  fs.writeFile(filePath, result, 'utf8', function(err) {
-    if (err) return console.log(err)
+fs.readdir('./out', function(err, files) {
+  const fileList = files.filter(function(file) {
+    return fs.statSync('out/' + file).isFile() && /precache*/.test(file) //絞り込み
+  })
+  fileList.forEach(fileName => {
+    replaceStatic('out/' + fileName)
   })
 })
+
+const buildBMPath = 'out/build-manifest.json'
+replaceStatic(buildBMPath)
